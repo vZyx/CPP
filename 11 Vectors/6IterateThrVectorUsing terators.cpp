54aa1b3d788d16defad74bf62e30b2ab1b1e04fc
Ziyad saeed Alghamdi <ziyadsaeedgh@gmail.com>
2023-03-25 01:00:38 +0300

    oop

--- 11 Vectors/6IterateThrVectorUsing terators.cpp
+++ 11 Vectors/6IterateThrVectorUsing terators.cpp
@@ -6,13 +6,19 @@ int main() {
 
     vector<int> num {1, 2, 3, 4, 5};
     // declare iterator
-    vector<int>::iterator iter;
+    vector<int>::iterator itr;
 
     // use iterator with for loop
-    for (iter = num.begin(); iter != num.end(); ++iter) {
-      cout << *iter << "  ";
+    for (itr = num.begin(); itr != num.end(); ++itr) {
+      cout << *itr << "  ";
     }
     /*we have used a for loop to initialize and iterate the iterator iter from the beginning of the vector
-    to the end of the vector using the begin() and end() functions*/
+    to the end of the vector using the begin() and end() functions
+
+    another way,
+    for (int i=0; i < num.size(); i++ ){
+      cout << num[i] << "  ";
+    }
+*/
     return 0;
 }
